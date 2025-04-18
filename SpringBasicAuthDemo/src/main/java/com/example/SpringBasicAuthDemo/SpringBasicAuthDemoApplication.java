65a4227b447aecc49d1880107be26828dedf859e
sayancoding <sayanmaity007@gmail.com>
2025-04-18 11:57:29 +0530

    Basic Auth initiated

---
+++ SpringBasicAuthDemo/src/main/java/com/example/SpringBasicAuthDemo/SpringBasicAuthDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBasicAuthDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicAuthDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasicAuthDemoApplication.class, args);
+	}
+
+}
---
+++ SpringBasicAuthDemo/src/test/java/com/example/SpringBasicAuthDemo/SpringBasicAuthDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBasicAuthDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicAuthDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
